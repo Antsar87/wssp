d2de53c958a7bffae6c59bb8da609e6c00de14de
antsar <jomay1129@gmail.com>
2022-07-13 19:16:20 -0600

    finished

--- ts/index.ts
+++ ts/index.ts
@@ -3,9 +3,27 @@ const ddi = <HTMLInputElement>document.getElementById('ddi'); //Primer Input
 const ddd = <HTMLInputElement>document.getElementById('ddd'); //Segundo
 const tel = <HTMLInputElement>document.getElementById('tel'); //Tercero
 
-form?.addEventListener('submit', () => {
+// wssp
+const wssp = document.getElementById('wssp');
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault()
   window.open(
     `https://api.whatsapp.com/send?phone=${ddi.value}${ddd?.value}${tel.value}&text=Olá`,
     '_blank'
   );
+});
+
+wssp?.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (ddi.value === '' || tel.value === '') {
+    alert('found number is empty');
+    return;
+  }
+
+  window.open(
+    `https://api.whatsapp.com/send?phone=${ddi.value}${ddd?.value}${tel.value}&text=https://zapsemsalvar.online/`,
+    '_blank'
+  );
+ 
 });
